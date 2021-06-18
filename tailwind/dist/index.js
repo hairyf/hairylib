aee0815dfdde11834ab965c4a9a29891ddf2b35b
TuiMao233 <wwu710632@gmail.com>
2021-06-18 17:32:32 +0800

    🎨 优化安装速度。

--- tailwind/dist/index.js
+++ tailwind/dist/index.js
@@ -2,7 +2,7 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-06-18 14:02:44
- * @LastEditTime: 2021-06-18 15:23:19
+ * @LastEditTime: 2021-06-18 17:30:43
  * @Description:
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -10,7 +10,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const lodash_1 = require("lodash");
 const defaults_1 = require("./defaults");
-console.log(defaults_1.defaults);
 /**
  * 初始化返回预设
  * @param config 深层合并预设
