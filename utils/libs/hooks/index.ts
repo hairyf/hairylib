9e4e0db68614c53620befbf726417edc73921868
TuiMao233 <wwu710632@gmail.com>
2021-06-30 21:29:59 +0800

    🔧 修复引入错误。

--- utils/libs/hooks/index.ts
+++ utils/libs/hooks/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-06-28 21:49:58
- * @LastEditTime: 2021-06-28 21:50:20
+ * @LastEditTime: 2021-06-30 21:29:02
  * @Description: 
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -11,3 +11,4 @@ export * from './use-count-down'
 export * from './use-historys'
 export * from './use-list-pagination'
 export * from './use-model-ref'
+export * from './use-multiple-select'
