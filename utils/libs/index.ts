d0db32cd17fa4e56bb63116f3dc97eb170a83023
TuiMao233 <wwu710632@gmail.com>
2021-07-13 17:24:07 +0800

    📦 新增内容。

--- utils/libs/index.ts
+++ utils/libs/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-06-28 16:33:40
- * @LastEditTime: 2021-07-12 15:22:09
+ * @LastEditTime: 2021-07-13 17:20:06
  * @Description: 
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -10,3 +10,4 @@ export * from './axios'
 export * from './browser'
 export * from './common'
 export * from './vue-hooks'
+export * from './vue-utils'
--- utils/package/index.d.ts
+++ utils/package/index.d.ts
@@ -2,3 +2,4 @@ export * from './axios';
 export * from './browser';
 export * from './common';
 export * from './vue-hooks';
+export * from './vue-utils';
