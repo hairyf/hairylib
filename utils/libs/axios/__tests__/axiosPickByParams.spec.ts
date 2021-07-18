2961d880b9291618d0962c7349f598c4be5db4e1
TuiMao233 <wwu710632@gmail.com>
2021-07-18 15:12:07 +0800

    ✨ 新增内容。

--- utils/libs/axios/__tests__/axiosPickByParams.spec.ts
+++ utils/libs/axios/__tests__/axiosPickByParams.spec.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-07-18 09:14:18
- * @LastEditTime: 2021-07-18 09:51:36
+ * @LastEditTime: 2021-07-18 15:11:06
  * @Description: 
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -32,4 +32,18 @@ describe('axiosPickByParams', () => {
       })
   })
 
+  it('axios pickby FormData', (done) => {
+    const http = axios.create()
+    axiosPickByParams(http, ['', undefined])
+    const formData = new FormData()
+    formData.append('aaa', '')
+    formData.append('bbb', undefined)
+    formData.append('ccc', '123131')
+    console.log(formData.values())
+    http.post('xxx')
+      .catch(v => {
+        expect(v.config.data).toEqual(v.config.data)
+        done()
+      })
+  })
 })
