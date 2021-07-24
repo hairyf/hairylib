28376c03afca2972896a38d05d762b9cf68b53c8
TuiMao233 <wwu710632@gmail.com>
2021-07-25 00:03:37 +0800

    ✨ 整理文档。

--- eslint/config/typescript.js
+++ eslint/config/typescript.js
@@ -2,7 +2,7 @@
  * @Author: Mr.Mao
  * @Date: 2021-01-07 10:12:14
  * @LastEditTime: 2021-06-08 11:30:39
- * @Description:
+ * @Description: typescript 基础配置
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
  */
--- eslint/config/vue@3.js
+++ eslint/config/vue@3.js
@@ -3,7 +3,7 @@
  * @LastEditors: Mr.Mao
  * @Date: 2021-01-07 10:12:14
  * @LastEditTime: 2021-06-08 14:36:19
- * @Description:
+ * @Description: 该配置必须得与 ['vue-basic'] 组合
  * 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
  */
 module.exports = {
