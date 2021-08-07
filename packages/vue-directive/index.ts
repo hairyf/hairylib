c5dce5cf10c7ddc6798af600b0d2ba6ada3e18da
TuiMao233 <wwu710632@gmail.com>
2021-08-08 02:51:06 +0800

    ✨ 更新内容。

--- packages/vue-directive/index.ts
+++ packages/vue-directive/index.ts
@@ -1,13 +1,19 @@
 import { App } from 'vue'
-import * as directives from './directives'
+import { Directive } from 'vue-demi'
+import * as _directives from './directives'
+
+const directives: Record<string, Directive<any>> = {}
+for (const [key, value] of Object.entries(_directives)) {
+  if (!key.startsWith('v')) continue
+  const directive = Object.entries(value)[0]
+  directives[directive[0]] = directive[1]
+}
 
 const install = (app: App) => {
-  for (const [key, value] of Object.entries(directives)) {
-    if (!key.startsWith('v')) continue
-    const directive = Object.entries(value)[0]
-    app.directive(directive[0], directive[1])
+  for (const [_k, _d] of Object.entries(directives)) {
+    app.directive(_k, _d)
   }
 }
 
 export default install
-export { install }
+export { install, directives }
---
+++ packages/vue-directive/v-grid/index.ts
@@ -0,0 +1,5 @@
+import { directiveUnit } from '../unit'
+
+export const vGap = { gap: directiveUnit('gap') }
+export const vGapX = { 'gap-x': directiveUnit('columnGap') }
+export const vGapY = { 'gap-y': directiveUnit('rowGap') }
