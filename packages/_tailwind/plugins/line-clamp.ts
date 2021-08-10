a0e8fdb44ca1e5cb6672162d1e79a8d7989692a5
TuiMao233 <wwu710632@gmail.com>
2021-08-10 10:44:59 +0800

    🎨 更新 tailwind 配置。

--- packages/_tailwind/plugins/line-clamp.ts
+++ packages/_tailwind/plugins/line-clamp.ts
@@ -5,7 +5,7 @@ export const lineClampPlugin = () => {
   return plugin(({ addUtilities, theme }: any) => {
     const newUtils = generateArray(1, theme('line-clamp') || 4).reduce((total, value) => {
       total[`.line-clamp-${value}`] = {
-        '-webkit-line-clamp': value
+        '-webkit-line-clamp': `${value}`
       }
       return total
     }, {} as Record<string, any>)
