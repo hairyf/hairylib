70aa9130f479d3b6148eaa1ed7e24dbdae94706c
TuiMao233 <wwu710632@gmail.com>
2021-08-10 09:36:46 +0800

    ✨ 更新配置，修复 tailwind 插件。

--- packages/_tailwind/plugins/line-clamp.ts
+++ packages/_tailwind/plugins/line-clamp.ts
@@ -4,12 +4,12 @@ const plugin = require('tailwindcss/plugin')
 export const lineClampPlugin = () => {
   return plugin(({ addUtilities, theme }: any) => {
     const newUtils = generateArray(1, theme('line-clamp') || 4).reduce((total, value) => {
-      total[`line-clamp-${value}`] = {
+      total[`.line-clamp-${value}`] = {
         '-webkit-line-clamp': value
       }
       return total
     }, {} as Record<string, any>)
-    newUtils['line-clamp'] = {
+    newUtils['.truncate-clamp'] = {
       display: '-webkit-box',
       '-webkit-box-orient': 'vertical',
       overflow: 'hidden'
