76ede2f595c3307dcc89ae0fd66175713af1b343
Mr.Mao <951416545@qq.com>
2021-10-28 17:18:49 +0800

    chore: add @commitlint/config-lerna-scopes extends

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ['@commitlint/config-lerna-scopes', '@commitlint/config-conventional'],
   rules: {
     'type-enum': [
       2,
