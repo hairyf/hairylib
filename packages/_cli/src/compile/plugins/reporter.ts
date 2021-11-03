9e43e23975e4edf50a2e130e1ada3d17cbe21c27
Mr.Mao <951416545@qq.com>
2021-11-03 17:42:50 +0800

    feat: optimization @hairy/cli:logger

--- packages/_cli/src/compile/plugins/reporter.ts
+++ packages/_cli/src/compile/plugins/reporter.ts
@@ -15,24 +15,25 @@ export const reporterPlugin = (mode: string): Plugin => {
     setup: ({ onResolve, onStart, onEnd }) => {
       onStart(() => {
         console.log(
-          chalk.cyan(`@hairy/cli v${version} ${chalk.green(`building for ${mode}...`)}\n\n`)
+          chalk.cyan(`@hairy/cli v${version} ${chalk.green(`building for ${mode}...`)}\n`)
         )
       })
       onEnd(() => {
         if (mode !== 'development') {
           const endTime = (Date.now() - startTime) / 1000
-          console.log(chalk.cyan(`sfe-library built in ${chalk.yellow(endTime + 's')}\n`))
+          console.log(chalk.cyan(`sfe-library built in ${chalk.yellow(endTime + 's')}`))
         }
       })
       const texts: string[] = []
+      let remaining = 0
       onResolve({ filter: /.*/ }, (options): any => {
-        if (texts.length < 3) {
+        if (texts.length <= 2) {
           texts.push(`transforming ${chalk.magenta(options.path)}`)
         }
-        if (texts.length === 3) {
-          texts.push(`transforming remaining files...`)
+        if (texts.length >= 3) {
+          texts[3] = `transforming remaining ${chalk.cyan(remaining++)} files...`
         }
-        if (texts.length > 0) writeLogger(texts.join('\n'))
+        if (texts.length > 0) writeLogger(texts.join('\n') + '\n')
       })
     }
   }
