731d91bff75db2a5eaf2ea8056b903bef82d41bb
wuweijian <tuimao.wu@ebgfx.com>
2021-12-17 17:57:22 +0800

    fix: @hairy/vue-directive v-has error

--- packages/vue-directive/v-utils/index.ts
+++ packages/vue-directive/v-utils/index.ts
@@ -1,13 +1,21 @@
+/*
+ * @Author: Mr'Mao https://github.com/TuiMao233
+ * @Date: 2021-12-06 18:13:53
+ * @LastEditors: Mr'Mao
+ * @LastEditTime: 2021-12-17 17:47:49
+ */
+import { isArray } from 'lodash'
 import { DirectiveElements, directiveOptions } from '../utils'
 export const vUtils: DirectiveElements = {
   focus: (element) => {
     if (element.tagName === 'input') element.focus()
     else element.querySelector('input')?.focus()
   },
-  has: (element, { value }) => {
-    const { permissions } = directiveOptions
-    if (value && permissions.includes(value)) {
-      element.parentNode && element.remove()
+  has: (element, { value }: { value: string[] }) => {
+    if (!isArray(value)) {
+      throw new TypeError(`v-has value: ${value} is not Array.`)
     }
+    const through = value.some((string_) => directiveOptions.permissions.includes(string_))
+    if (!through) element.parentNode && element.remove()
   }
 }
