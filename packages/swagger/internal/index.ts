ccc4d53f34bb63df3e7648557685d85a00b5eaf2
wuweijian <tuimao.wu@ebgfx.com>
2021-12-29 11:09:06 +0800

    feat: add swagger internal

--- packages/swagger/internal/index.ts
+++ packages/swagger/internal/index.ts
@@ -2,5 +2,16 @@
  * @Author: Mr'Mao https://github.com/TuiMao233
  * @Date: 2021-12-29 11:03:27
  * @LastEditors: Mr'Mao
- * @LastEditTime: 2021-12-29 11:03:28
+ * @LastEditTime: 2021-12-29 11:08:11
  */
+
+/** swagger 类型 typescript 映射 */
+export const TYPE_MAPPING = {
+  integer: 'number',
+  TypesLong: 'number',
+  TypesString: 'string',
+  object: 'Record<string, any>'
+}
+
+/** api 导入 ts 类型集合的 命名空间名字 */
+export const TS_TYPE_NAME_SPACE = 'SwaggerType'
