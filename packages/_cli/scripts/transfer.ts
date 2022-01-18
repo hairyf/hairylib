9db7b0896d995c6d783c97abb3901e27f50cd691
Mr.Mao <951416545@qq.com>
2022-01-18 14:20:16 +0800

    fix: @hairy/cli build error

--- packages/_cli/scripts/transfer.ts
+++ packages/_cli/scripts/transfer.ts
@@ -1,7 +1,7 @@
 import fs from 'fs-extra'
 import path from 'path'
-fs.ensureDirSync(path.resolve(__dirname, '../dist/create/template'))
+fs.ensureDirSync(path.resolve(__dirname, '../dist/commands/create/template'))
 fs.copy(
-  path.resolve(__dirname, '../src/create/template'),
-  path.resolve(__dirname, '../dist/create/template')
+  path.resolve(__dirname, '../src/commands/create/template'),
+  path.resolve(__dirname, '../dist/commands/create/template')
 )
