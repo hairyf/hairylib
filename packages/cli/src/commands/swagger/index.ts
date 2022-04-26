01cb8d7f919f7958655f02e157d9fe47bc225b13
mao <951416545@qq.com>
2022-04-26 16:10:48 +0800

    feat: remove cli swagger dependencies

--- packages/cli/src/commands/swagger/index.ts
+++ packages/cli/src/commands/swagger/index.ts
@@ -1,9 +1,8 @@
-import { SwaggerDefineConfig } from '@hairy/swagger'
 import { loadConfigFromFile } from './utils'
 
 export const actionSwagger = async () => {
   const { swaggerWebClientGenerator } = require('@hairy/swagger')
-  const { config = {} } = await loadConfigFromFile<SwaggerDefineConfig>('swagger.config')
+  const { config = {} } = await loadConfigFromFile('swagger.config')
   const servers = config.servers || []
 
   delete config.servers
