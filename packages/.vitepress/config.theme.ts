ae3a3192bf30160e4a6e50a2b02acfe6d9e01cc7
mao <951416545@qq.com>
2022-04-26 14:41:09 +0800

    feat: add vitepress action cname plugin

---
+++ packages/.vitepress/config.theme.ts
@@ -0,0 +1,76 @@
+import { DefaultTheme } from "vitepress"
+import { camelCase } from 'lodash'
+import indexes, { categories } from '../indexes.json'
+
+const Guide = [
+  { text: 'Get Started', link: '/guide/' },
+  { text: 'Contributing', link: '/guide/contributing' },
+]
+
+const CoreCategories = categories.map(c => ({
+  text: capitalizeCamelCase(c),
+  link: indexes.documents.find(item => item.category === c)?.docs || ''
+}))
+
+const DefaultSideBar: DefaultTheme.SideBarConfig = [
+  { text: 'Guide', children: Guide },
+  { text: 'Core Functions', children: CoreCategories },
+]
+
+const DocumentsSideBar = getDocumentSideBar()
+
+export const sidebar: DefaultTheme.SideBarConfig = {
+  '/guide/': DefaultSideBar,
+  '/contributing': DefaultSideBar,
+
+  // #region functions
+  ...getReduceSideBar(indexes.documents.filter(item => item.name === item.package).map(item => item.name)),
+  ...getReduceSideBar(categories),
+  // #endregion
+}
+
+export const nav: DefaultTheme.NavItem[] = [
+  {
+    text: 'Guide',
+    items: Guide
+  },
+  {
+    text: 'Functions',
+    items: CoreCategories
+  },
+  {
+    text: 'Github',
+    link: 'https://github.com/TuiMao233/hairylib',
+  },
+  {
+    text: 'Blog',
+    link: 'https://tuimao233.gitee.io/mao-blog/',
+  },
+]
+
+function capitalizeCamelCase (string_: string) {
+  let result = camelCase(string_)
+  result = result.slice(0, 1).toLocaleUpperCase() + result.slice(1)
+  return result
+}
+
+function getDocumentSideBar() {
+  const links = []
+  for (const name of categories) {
+    const documents = indexes.documents.filter(i => i.category === name)
+    links.push({
+      text: capitalizeCamelCase(name),
+      children: documents.map(i => ({ text: i.name, link: i.docs })),
+    })
+  }
+  return links
+}
+
+function getReduceSideBar(names: string[]) {
+  return names.reduce((total, key) => {
+    if (!key) return total
+    const path = `/${key?.toLowerCase()}/`
+    total[path] = DocumentsSideBar
+    return total
+  }, <any>{})
+}
--- packages/.vitepress/config.ts
+++ packages/.vitepress/config.ts
@@ -4,27 +4,10 @@
  * @LastEditors: Mr'Mao
  * @LastEditTime: 2022-01-22 20:10:48
  */
-import { defineConfig, DefaultTheme } from 'vitepress'
-import { camelCase } from 'lodash'
-import indexes, { categories } from '../indexes.json'
+import { defineConfig } from 'vitepress'
+import {sidebar, nav} from './config.theme'
+import cname from './plugins/vite-plugin-cname'
 
-const Guide = [
-  { text: 'Get Started', link: '/guide/' },
-  { text: 'Contributing', link: '/guide/contributing' },
-]
-
-const CoreCategories = categories.map(c => ({
-  text: capitalizeCamelCase(c),
-  link: indexes.documents.find(item => item.category === c)?.docs || ''
-}))
-
-const DefaultSideBar: DefaultTheme.SideBarConfig = [
-  { text: 'Guide', children: Guide },
-  { text: 'Core Functions', children: CoreCategories },
-] as any
-
-
-const DocumentsSideBar = getDocumentSideBar()
 
 const config = defineConfig({
   head: [
@@ -32,62 +15,12 @@ const config = defineConfig({
   ],
   title: 'Hairy Library',
   description: 'Hairy Library',
-  themeConfig: {
-    logo: '/favicon.svg',
-    sidebar: {
-      '/guide/': DefaultSideBar,
-      '/contributing': DefaultSideBar,
-
-      // #region functions
-      ...getReduceSideBar(indexes.documents.filter(item => item.name === item.package).map(item => item.name)),
-      ...getReduceSideBar(categories),
-      // #endregion
-    },
-    nav: [
-      {
-        text: 'Guide',
-        items: Guide
-      },
-      {
-        text: 'Functions',
-        items: CoreCategories
-      },
-      {
-        text: 'Github',
-        link: 'https://github.com/TuiMao233/hairylib',
-      },
-      {
-        text: 'Blog',
-        link: 'https://tuimao233.gitee.io/mao-blog/',
-      },
-    ]
-  },
-})
-function capitalizeCamelCase (string_: string) {
-  let result = camelCase(string_)
-  result = result.slice(0, 1).toLocaleUpperCase() + result.slice(1)
-  return result
-}
-
-function getDocumentSideBar() {
-  const links = []
-  for (const name of categories) {
-    const documents = indexes.documents.filter(i => i.category === name)
-    links.push({
-      text: capitalizeCamelCase(name),
-      children: documents.map(i => ({ text: i.name, link: i.docs })),
-    })
+  themeConfig: { logo: '/favicon.svg', sidebar, nav },
+  vite: {
+    plugins: [cname()]
   }
-  return links
-}
+})
 
-function getReduceSideBar(names: string[]) {
-  return names.reduce((total, key) => {
-    if (!key) return total
-    const path = `/${key?.toLowerCase()}/`
-    total[path] = DocumentsSideBar
-    return total
-  }, <any>{})
-}
+
 
 export default config
