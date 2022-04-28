1964349e4d04360ba6157d423598cbeb4fecb4e5
mao <951416545@qq.com>
2022-04-28 10:08:53 +0800

    feat: publish eslint 1.0.30

--- packages/eslint/src/rules/utils.ts
+++ packages/eslint/src/rules/utils.ts
@@ -1,7 +1,25 @@
+import { mergeWith } from 'lodash'
+import { unicorn } from '.'
+import { mergeCustomizer } from '../utils'
+
 /**
  * unicorn/prevent-abbreviations 白名单
  * @param name
  */
 export const preventAbbreviationsWhites = (names: string[]) => {
   return names.flatMap((v) => [new RegExp(v, 'i'), new RegExp(v + 's', 'i')])
+}
+
+/**
+ * 继承 @hairy/eslint unicorn/prevent-abbreviations 并覆盖
+ * @param names
+ */
+export const extendsPreventAbbreviations = (names: string[]) => {
+  const options = { ignore: preventAbbreviationsWhites(names) }
+  return {
+    'unicorn/prevent-abbreviations': [
+      unicorn['unicorn/prevent-abbreviations'][0],
+      mergeWith(options, unicorn['unicorn/prevent-abbreviations'][1], mergeCustomizer)
+    ]
+  }
 }
