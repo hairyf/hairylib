c2cbf0415e7fc6790e0ad1574977f8d847c0f794
Mr.Mao <951416545@qq.com>
2024-05-31 15:54:28 +0800

    feat: update react-utils and core typings and format

--- packages/react-utils/src/types/index.ts
+++ packages/react-utils/src/types/index.ts
@@ -1,3 +1,5 @@
 import type { HTMLAttributes, DetailedHTMLProps as _DetailedHTMLProps } from 'react'
 
 export type DetailedHTMLProps<T = HTMLDivElement> = _DetailedHTMLProps<HTMLAttributes<T>, T>
+
+export type BooleanLike = any
