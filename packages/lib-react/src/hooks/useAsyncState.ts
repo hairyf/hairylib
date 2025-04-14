7528a606fa878e695f746258ef1b6d2c7a1118bc
hairyf <wwu710632@gmail.com>
2025-04-14 15:15:50 +0800

    fix(hooks): update useAsyncState to accept dependencies and ensure immediate execution

--- packages/lib-react/src/hooks/useAsyncState.ts
+++ packages/lib-react/src/hooks/useAsyncState.ts
@@ -1,26 +1,30 @@
 import type { AnyFn, PromiseFn, PromiseType } from '@hairy/utils'
 import type { AsyncStateReturn } from './types'
 import { useEffect, useState } from 'react'
-import { useMount } from 'react-use'
 import { useAsyncCallback } from './useAsyncCallback'
 
 export type UseAsyncStateOptions<T extends AnyFn> = {
   immediate?: boolean
   initial?: PromiseType<ReturnType<T>>
+  deps?: any[]
 } | {
   immediate?: boolean
   initial: PromiseType<ReturnType<T>>
+  deps?: any[]
 }
 
 export function useAsyncState<T extends PromiseFn>(
   fun: T,
+  deps: any[] = [],
   options?: UseAsyncStateOptions<T>,
 ): AsyncStateReturn<T> {
   const [value, set] = useState(options?.initial)
   const [loading, execute, error] = useAsyncCallback(async (...args: any[]) => fun(...args).then(set))
-  useMount(() => options?.immediate && execute())
-  useEffect(() => {
-    execute()
-  }, [execute])
+  useEffect(
+    () => {
+      execute()
+    },
+    deps,
+  )
   return [{ value, loading, error }, execute] as any
 }
