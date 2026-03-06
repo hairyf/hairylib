baa591d74765fec5dd2ccbe63a1212e221a2459c
hairyf <wwu710632@gmail.com>
2026-03-06 17:15:56 +0800

    fix(util): improve ghost function by extracting resolve logic for better clarity

--- packages/util-core/src/util/ghost.ts
+++ packages/util-core/src/util/ghost.ts
@@ -8,8 +8,12 @@ export type Ghost<T extends object> = T & {
 export function ghost<T extends object>(strictMessage?: string): Ghost<T> {
   const placeholder = proxy<Ghost<T>>(
     undefined,
-    { enable: false, resolve: (value: T) => placeholder.proxy.update(value as any) },
+    { enable: false, resolve },
     { strictMessage: strictMessage || 'Object is not enabled. Call ghost.resolve(value) to enable the object.' },
   )
+  function resolve(value: T) {
+    placeholder.proxy.update(value as any)
+    placeholder.enabled = true
+  }
   return placeholder
 }
