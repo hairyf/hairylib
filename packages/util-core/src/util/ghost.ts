0e2261900da974e6d68bd3de70cc536246908e2c
hairyf <wwu710632@gmail.com>
2026-03-06 17:29:26 +0800

    fix(util): correct property name from 'enable' to 'enabled' in ghost function

--- packages/util-core/src/util/ghost.ts
+++ packages/util-core/src/util/ghost.ts
@@ -8,7 +8,7 @@ export type Ghost<T extends object> = T & {
 export function ghost<T extends object>(strictMessage?: string): Ghost<T> {
   const placeholder = proxy<Ghost<T>>(
     undefined,
-    { enable: false, resolve },
+    { enabled: false, resolve },
     { strictMessage: strictMessage || 'Object is not enabled. Call ghost.resolve(value) to enable the object.' },
   )
   function resolve(value: T) {
